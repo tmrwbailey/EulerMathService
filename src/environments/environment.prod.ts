01216826c3f4d84128a83e49a6e45b0d8f263ed8
Mark Baiey <supermathnerd@gmail.com>
2024-12-31 16:07:26 -0600

    Correct azure service api address.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    calcApiUrl: 'https://extremeprecisioncalcapi.azure-api.net'
+    calcApiUrl: 'https://eulermathservice.azurewebsites.net/'
 }
